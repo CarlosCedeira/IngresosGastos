01cb876f9150d7cb7b2cdddea95f9175eba68b17
CarlosCedeira <cedeira05@gmail.com>
2024-09-15 05:06:21 +0200

    Deploy build output to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ingresosGastos/", // Nombre de tu repositorio en GitHub
   server: {
     host: "0.0.0.0",
   },
